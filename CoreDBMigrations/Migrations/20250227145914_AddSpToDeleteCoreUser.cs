44262aa1624082f7b826586f142c0bbd7635f10e
SoSa-HEIDI <sonu@heidi-app.de>
2025-02-27 16:44:35 +0100

    removed bookings table from stored procedure

--- CoreDBMigrations/Migrations/20250227145914_AddSpToDeleteCoreUser.cs
+++ CoreDBMigrations/Migrations/20250227145914_AddSpToDeleteCoreUser.cs
@@ -28,7 +28,6 @@ namespace DatabaseMigrations.Migrations
                         DELETE FROM user_preference_categories upcat WHERE upcat.userId = userId;
                         DELETE FROM firebase_token ft WHERE ft.userId = userId;
 		                DELETE FROM favorites f WHERE f.userId = userId;
-                        DELETE FROM bookings b WHERE b.userId = userId;
                         DELETE FROM notification noti WHERE noti.userId = userId;
                         DELETE FROM user_keys uk WHERE uk.userId = userId;
                         DELETE FROM addresses add WHERE add.userId = userId;
