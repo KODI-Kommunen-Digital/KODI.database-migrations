c4a9701a6ce82785d147508be58d07ab2527e657
narenheidi <dev5@heidi-app.com>
2025-05-14 15:55:15 +0530

    removed foreign key (#124)
    Co-authored-by: spector <naren@ff.com>

--- CoreDBMigrations/Migrations/20250512070308_AddForumsPostTable.cs
+++ CoreDBMigrations/Migrations/20250512070308_AddForumsPostTable.cs
@@ -19,7 +19,6 @@ namespace DatabaseMigrations.Migrations
                     createdAt DATETIME,
                     status ENUM('Pending', 'Approved', 'Feedback')
                     isHidden tinyint(1),
-                    FOREIGN KEY(statusId) REFERENCES status(id),  
                     FOREIGN KEY(forumId) REFERENCES forums(id),    
                     FOREIGN KEY(userId) REFERENCES users(id)
                 );";
