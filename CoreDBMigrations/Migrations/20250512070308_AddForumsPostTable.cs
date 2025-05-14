9176e612beaffc27cdb5cc566d7d5aaafa69492d
spector <naren@ff.com>
2025-05-14 05:29:17 +0000

    added status here

--- CoreDBMigrations/Migrations/20250512070308_AddForumsPostTable.cs
+++ CoreDBMigrations/Migrations/20250512070308_AddForumsPostTable.cs
@@ -15,9 +15,11 @@ namespace DatabaseMigrations.Migrations
                     title varchar(255),
                     description varchar(10000),
                     userId int, 
+                    statusId int,
                     image varchar(255),
                     createdAt DATETIME,
                     isHidden tinyint(1),
+                    FOREIGN KEY(statusId) REFERENCES status(id),  
                     FOREIGN KEY(forumId) REFERENCES forums(id),    
                     FOREIGN KEY(userId) REFERENCES users(id)
                 );";
