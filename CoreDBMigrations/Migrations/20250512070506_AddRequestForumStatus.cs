f3a64728a05c78dd2901a02788aeaeffa2879616
narenheidi <dev5@heidi-app.com>
2025-05-14 11:14:52 +0530

    @forum/core db (#122)
    * forum table migrations for core db
    
    * foriegn keys moved to bottom
    
    * reason column for postreports
    
    * id removed from forum_cities and add feedback status to forum_status
    
    * consistent class name for migrations
    
    * added status here
    
    ---------
    
    Co-authored-by: spector <naren@ff.com>

--- CoreDBMigrations/Migrations/20250512070506_AddRequestForumStatus.cs
+++ CoreDBMigrations/Migrations/20250512070506_AddRequestForumStatus.cs
@@ -8,19 +8,19 @@ namespace DatabaseMigrations.Migrations
         public override void Up()
         {
             string sql =
-               @"DROP TABLE IF EXISTS forum_post_status;
-                CREATE TABLE forum_post_status (
+               @"DROP TABLE IF EXISTS forum_status;
+                CREATE TABLE forum_status (
                     id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
                     name varchar(255)
                 );
-                insert into forum_post_status values (1,""Pending""), (2,""Accepted""), (3,""Rejected""),(4,""Feedback"");";
+                insert into forum_status values (1,""Pending""), (2,""Accepted""), (3,""Rejected"");";
             Execute.Sql(sql);
         }
 
         public override void Down()
         {
             string sql =
-               @"DROP TABLE IF EXISTS forum_post_status;";
+               @"DROP TABLE IF EXISTS forum_status;";
 
             Execute.Sql(sql);
         }
