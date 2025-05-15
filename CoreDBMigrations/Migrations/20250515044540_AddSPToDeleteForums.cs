2a07b787e4580780e2099c18e2f5efc56f10bf90
spector <naren@ff.com>
2025-05-15 04:47:35 +0000

    stored procedure to delete forums from coredb

---
+++ CoreDBMigrations/Migrations/20250515044540_AddSPToDeleteForums.cs
@@ -0,0 +1,48 @@
+using FluentMigrator;
+
+namespace DatabaseMigrations.Migrations
+{
+    [Migration(20250515044540)]
+    public class AddSPToDeleteForums : Migration
+    {
+        public override void Up()
+        {   
+            /*
+                Changes made
+                --------------------------------------------------------------------------
+                2 August 2023 -> changed postReports to RepostedPosts - Sonu
+                25 Oct 2023 -> changed commentes to delete not null parents - Ajay
+            */
+            string sql =
+               @"
+                DROP PROCEDURE IF EXISTS sp_DeleteForums;
+                CREATE PROCEDURE sp_DeleteForums (IN forumId int) 
+                BEGIN
+                    DECLARE EXIT HANDLER FOR SQLEXCEPTION 
+                        BEGIN
+                            ROLLBACK;
+                            RESIGNAL;
+                        END;
+                    START TRANSACTION;
+                        DELETE FROM forum_comments fc where fc.forumId = forumId and fc.parentId is not NULL;
+                        DELETE FROM forum_comments fc where fc.forumId = forumId;
+                        DELETE FROM forum_post_reports pr where pr.forumId = forumId;
+                        DELETE FROM forum_posts fp where fp.forumId = forumId;
+                        DELETE FROM forum_requests fr where fr.forumId = forumId;
+                        DELETE FROM forum_members fm where fm.forumId = forumId;
+                        DELETE FROM forums fs where fs.id = forumId;
+                    COMMIT;
+                END;";
+
+            Execute.Sql(sql);
+        }
+
+        public override void Down()
+        {
+            string sql =
+                @"DROP PROCEDURE IF EXISTS sp_DeleteForums;";
+
+            Execute.Sql(sql);
+        }
+    }
+}
