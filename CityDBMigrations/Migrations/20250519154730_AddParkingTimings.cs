5747c3b16783cd0bc7e2287536f84f4cf371b6ea
sameer-heidi <dev8@heidi-app.com>
2025-05-19 18:15:42 +0530

    updated CarparkList file and added ParkingTimings list

---
+++ CityDBMigrations/Migrations/20250519154730_AddParkingTimings.cs
@@ -0,0 +1,30 @@
+using FluentMigrator;
+
+namespace DatabaseMigrations.Migrations
+{
+    [Migration(20250519154500)]
+    public class CreateParkingTimingsTable : Migration
+    {
+        public override void Up()
+        {
+            Execute.Sql(@"
+                CREATE TABLE `parking_timings` (
+                    `day` TINYINT NOT NULL,
+                    `parking_id` VARCHAR(100) NOT NULL,
+                    `startTime` DATETIME DEFAULT NULL,
+                    `endTime` DATETIME DEFAULT NULL,
+                    PRIMARY KEY (`parking_id`, `day`),
+                    CONSTRAINT `parking_timings_ibfk_1` 
+                        FOREIGN KEY (`parking_id`) REFERENCES `carparklist` (`id`) ON DELETE CASCADE,
+                    CONSTRAINT `parking_timings_chk_1` 
+                        CHECK ((`day` BETWEEN 0 AND 6))
+                ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+            ");
+        }
+
+        public override void Down()
+        {
+            Execute.Sql("DROP TABLE IF EXISTS `parking_timings`;");
+        }
+    }
+}
