1a03a63a3b8e45d5f2e154f9601631e6ffefcbbd
narenheidi <dev5@heidi-app.com>
2025-06-04 09:59:37 +0530

    Formstatus (#140)
    * add status for forums table for admin view
    
    * support for chat inhancment for fourms
    
    ---------
    
    Co-authored-by: spector <naren@ff.com>

---
+++ CoreDBMigrations/Migrations/20250604042503_AddColumnsToForumChats.cs
@@ -0,0 +1,29 @@
+using FluentMigrator;
+
+namespace DatabaseMigrations.Migrations
+{
+    [Migration(20250520123146)]
+    public class AddColumnsToForumChats : Migration
+    {
+        public override void Up()
+        {
+            string sql =
+               @"ALTER TABLE forum_chats
+                ADD COLUMN parentId BIGINT NULL,
+                ADD COLUMN fileUrl VARCHAR(255) NULL,
+                ADD FOREIGN KEY (parentId) REFERENCES forum_chats(id) ON DELETE CASCADE;";
+            Execute.Sql(sql);
+        }
+
+        public override void Down()
+        {
+            string sql =
+               @"ALTER TABLE forum_chats
+                DROP FOREIGN KEY forum_chats_ibfk_2,
+                DROP COLUMN parentId,
+                DROP COLUMN fileUrl;";
+
+            Execute.Sql(sql);
+        }
+    }
+} 
