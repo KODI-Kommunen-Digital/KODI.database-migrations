4cbd1969741324185c5941321a2940a01bd3e458
spector <naren@ff.com>
2025-06-04 04:26:11 +0000

    support for chat inhancment for fourms

---
+++ CoreDBMigrations/Migrations/20250604042533_AddForumChatReactionTable.cs
@@ -0,0 +1,34 @@
+using FluentMigrator;
+
+namespace DatabaseMigrations.Migrations
+{
+    [Migration(20250520123147)]
+    public class AddForumChatReactionTable : Migration
+    {
+        public override void Up()
+        {
+            string sql =
+               @"DROP TABLE IF EXISTS forum_chat_reactions;
+                CREATE TABLE `forum_chat_reactions` (
+                id BIGINT NOT NULL AUTO_INCREMENT,
+                chatId BIGINT NOT NULL,
+                userId INT NOT NULL,
+                reaction ENUM('like', 'dislike') NOT NULL,
+                createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                PRIMARY KEY (`id`),
+                UNIQUE KEY `unique_reaction_per_user` (`chatId`, `userId`),
+                FOREIGN KEY (`chatId`) REFERENCES `forum_chats` (`id`) ON DELETE CASCADE,
+                FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE
+                );";
+            Execute.Sql(sql);
+        }
+
+        public override void Down()
+        {
+            string sql =
+               @"DROP TABLE IF EXISTS forum_chat_reactions;";
+
+            Execute.Sql(sql);
+        }
+    }
+} 
